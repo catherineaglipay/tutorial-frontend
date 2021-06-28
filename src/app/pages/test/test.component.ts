bbf94987b491a3a6e82cd9477343a21712799ba7
Catherine Aglipay <catherine.aglipay@valueline.com.ph>
2021-06-29 01:10:35 +0800

    edit and delete

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -13,6 +13,7 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
 })
 export class TestComponent extends BaseComponent implements OnInit {
     @ViewChild("addStudentModal") public addStudentModal: ModalDirective
+    @ViewChild("editStudentModal") public editStudentModal: ModalDirective
 
     global_var = this.test_global_var
     global_func = this.testGlobalFunction()
@@ -22,6 +23,11 @@ export class TestComponent extends BaseComponent implements OnInit {
     modal_first_name = ""
     modal_middle_name = ""
     modal_last_name = ""
+
+    modal_edit_student_key = 0
+    modal_edit_first_name = ""
+    modal_edit_middle_name = ""
+    modal_edit_last_name = ""
 
     test_array: Array<any> = [
         {
@@ -89,7 +95,7 @@ export class TestComponent extends BaseComponent implements OnInit {
             "middle_name"       : this.modal_middle_name,
             "last_name"         : this.modal_last_name
         }).subscribe(backend_response => {
-            console.log("backend response: ", backend_response)
+            // console.log("backend response: ", backend_response)
             if (backend_response.statusCode == 200) {
                 Swal.fire({
                     title: "Success!",
@@ -100,10 +106,12 @@ export class TestComponent extends BaseComponent implements OnInit {
                     confirmButtonText: "Confirm"
                 }).then((result) => {
                     if (result.value) {
+                        this.ngOnInit()
                         this.addStudentModal.hide()
                     }
                 })
                 console.log("success add: ", backend_response)
+                
             } else {
                 Swal.fire({
                     title: "Oops!",
@@ -129,5 +137,107 @@ export class TestComponent extends BaseComponent implements OnInit {
     }
 
 
+    editStudent(param_student_data) {
+        console.log("edit student data: ", param_student_data)
+        this.modal_edit_student_key = param_student_data.key
+        this.modal_edit_first_name = param_student_data.fname
+        this.modal_edit_middle_name = param_student_data.mname
+        this.modal_edit_last_name = param_student_data.lname
+    }
+
+
+    updateStudent() {
+        this.API.post("/parent-route/update-student", {
+            "key"               : this.modal_edit_student_key, 
+            "first_name"        : this.modal_edit_first_name, 
+            "middle_name"       : this.modal_edit_middle_name,
+            "last_name"         : this.modal_edit_last_name
+        }).subscribe(backend_response => {
+            // console.log("backend response: ", backend_response)
+            if (backend_response.statusCode == 200) {
+                Swal.fire({
+                    title: "Success!",
+                    text: backend_response.devMessage,
+                    icon: "success",
+                    allowOutsideClick: false,
+                    confirmButtonColor: "#00B5E2",
+                    confirmButtonText: "Confirm"
+                }).then((result) => {
+                    if (result.value) {
+                        this.ngOnInit()
+                        this.editStudentModal.hide()
+                    }
+                })
+                console.log("success update: ", backend_response)
+                
+            } else {
+                Swal.fire({
+                    title: "Oops!",
+                    text: backend_response.body.message,
+                    icon: "warning",
+                    allowOutsideClick: false,
+                    confirmButtonColor: "#00B5E2",
+                    confirmButtonText: "Confirm"
+                })
+                console.log("error", backend_response)
+            }
+        }, (error: any) => {
+            // Swal.fire({
+            //     title: "Oops!",
+            //     text: error.message,
+            //     icon: "warning",
+            //     allowOutsideClick: false,
+            //     confirmButtonColor: this.AMA_CUSTOM_PURPLE,
+            //     confirmButtonText: "Confirm"
+            // })
+            console.log(error)
+        },)
+    }
+
+
+    deleteStudent(param_student_key) {
+        this.API.post("/parent-route/delete-student", {
+            "key"               : param_student_key, 
+        }).subscribe(backend_response => {
+            // console.log("backend response: ", backend_response)
+            if (backend_response.statusCode == 200) {
+                Swal.fire({
+                    title: "Success!",
+                    text: backend_response.devMessage,
+                    icon: "success",
+                    allowOutsideClick: false,
+                    confirmButtonColor: "#00B5E2",
+                    confirmButtonText: "Confirm"
+                }).then((result) => {
+                    if (result.value) {
+                        this.ngOnInit()
+                        this.editStudentModal.hide()
+                    }
+                })
+                console.log("success delete: ", backend_response)
+                
+            } else {
+                Swal.fire({
+                    title: "Oops!",
+                    text: backend_response.body.message,
+                    icon: "warning",
+                    allowOutsideClick: false,
+                    confirmButtonColor: "#00B5E2",
+                    confirmButtonText: "Confirm"
+                })
+                console.log("error", backend_response)
+            }
+        }, (error: any) => {
+            // Swal.fire({
+            //     title: "Oops!",
+            //     text: error.message,
+            //     icon: "warning",
+            //     allowOutsideClick: false,
+            //     confirmButtonColor: this.AMA_CUSTOM_PURPLE,
+            //     confirmButtonText: "Confirm"
+            // })
+            console.log(error)
+        },)
+    }
 
 }
